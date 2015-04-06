06ddc771c43d35b5a1ce90f233e4b107cde956d7
haogj <haogj@hotmail.com>
2015-04-06 11:24:26 +0800

    Add other line.

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, git.");
+            Console.WriteLine("Add other line.");
         }
     }
 }
